c5b8583ade8339904055f83e44cea22f20e311a6
Deromad <119706549+Deromad@users.noreply.github.com>
2025-03-25 01:00:20 +0100

    wände der häuser sind jetzt flach

--- Scenes/simple_facade.gd
+++ Scenes/simple_facade.gd
@@ -7,6 +7,6 @@ func transform_obj(color: Color):
 	# create Material for each instance
 	var new_material = StandardMaterial3D.new()
 	new_material.albedo_color = color  
-
+	new_material.cull_mode = BaseMaterial3D.CULL_DISABLED
 	
 	mesh.set_surface_override_material(0, new_material)
